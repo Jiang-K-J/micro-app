bcb91c3172493e90f7183206bdc5d3eb7d8d1ba5
jam <143499168+Jiang-K-J@users.noreply.github.com>
2025-01-11 16:17:11 +0800

    Update connect2.jsx
    增加子应用间通信

--- example/qiankun/qiankun-rf/src/componets/connect2.jsx
+++ example/qiankun/qiankun-rf/src/componets/connect2.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Card, List, Button } from "antd";
 import { useQiankunProps } from "../QiankunContext";
+import { Input, message } from 'antd';
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [childMsg, setChildMsg] = useState("");
   const qiankunProps = useQiankunProps();
+  const [inputValue, setInputValue] = useState("我是 React 子应用，我给vue3子应用发信息");
 
   const mount = () => {
     console.log("子应用 mount", qiankunProps);
@@ -12,10 +15,23 @@ const App = () => {
     qiankunProps.actions.onGlobalStateChange((state, prev) => {
       console.log("子应用监听到状态变化", state, prev);
       setData(state.listData || []);
+      if(state.vueInfo){
+        setChildMsg(state.vueInfo)
+      }
     });
     qiankunProps.actions.setGlobalState({
         agency:"child-react"
       })
+  };
+
+  const handleClick = () => {
+    // 设置全局状态
+    qiankunProps.actions.setGlobalState({
+      info: inputValue,
+    });
+
+    // 显示提示信息
+    message.success('发送成功，快去vue3子应用看看吧');
   };
 
   useEffect(() => {
@@ -52,6 +68,20 @@ const App = () => {
       >
         向主应用发送信息
       </Button>
+      <div style={{display:"flex",marginTop:"10px"}}>
+      <span style={{minWidth:"70px"}}>编辑消息：</span>
+      <Input placeholder="Basic usage" value={inputValue} onChange={(event) =>
+          setInputValue(event.target.value)
+        } />
+      <Button
+        type="primary"
+        style={{marginLeft:"10px"}}
+        onClick={handleClick}
+      >
+        向vue3子应用通信
+      </Button>
+      </div>
+      <h3>接收子应用间通信的消息：{ childMsg || "子应用暂时为发送消息过来哦！" }</h3>
     </>
   );
 };
