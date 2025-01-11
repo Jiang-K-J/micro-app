e3caf6f4bf1f5e57c51363a00723591aea4b534d
jam <143499168+Jiang-K-J@users.noreply.github.com>
2025-01-11 16:08:04 +0800

    Update connect3.vue
    增加子应用间通信

--- example/qiankun/qiankun-v3/src/components/connect3.vue
+++ example/qiankun/qiankun-v3/src/components/connect3.vue
@@ -1,14 +1,19 @@
 <script setup>
 import { inject, onMounted, ref } from "vue";
+import { ElMessage } from 'element-plus'
 const listData = ref([]);
+const childMsg = ref("");
+const input = ref('我是 vue3 子应用，我给react子应用发信息')
 
 const qiankunProps = inject("qiankunProps");
 onMounted(() => {
   // 监听状态变化
-  console.log("子应用监听到状态变化2345", qiankunProps);
   qiankunProps.actions.onGlobalStateChange((state, prev) => {
     console.log("子应用监听到状态变化", state, prev);
     listData.value = state.listData || [];
+    if(state.info){
+      childMsg.value = state.info;
+    }
   });
   qiankunProps.actions.setGlobalState({
     agency: "child-react",
@@ -17,9 +22,19 @@ onMounted(() => {
 
 const handleClick = () => {
   qiankunProps.actions.setGlobalState({
-    msg: "我是 vue3 子应用，我给主应用发信息",
+    msg: input.value,
   });
 };
+
+const childrenClick = () => {
+  qiankunProps.actions.setGlobalState({
+    vueInfo: "我是 vue3 子应用，我给react子应用发信息",
+  });
+  ElMessage({
+    message: '传递成功，快去子应用看看吧',
+    type: 'success',
+  })
+};  
 </script>
 
 <template>
@@ -31,6 +46,11 @@ const handleClick = () => {
 
     <el-button type="primary" @click="handleClick">向主应用发送信息</el-button>
   </div>
+  <br/>
+  <span>编辑消息：</span>
+  <el-input v-model="input" style="width: 240px" placeholder="Please input" />&nbsp;
+  <el-button type="primary" @click="childrenClick">向react子应用发送信息</el-button>
+    <h3>接收子应用间通信的消息：{{ childMsg || "子应用暂时为发送消息过来哦！" }}</h3>
 </template>
 
 <style scoped>
